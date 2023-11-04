5530ac169632bad3387cab5c99968911ce1f924c
Andreas Bichinger <andreas.bichinger@gmail.com>
2023-11-04 17:38:59 +0100

    chore: switch from yarn to npm
    - update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,21 @@ import css from 'rollup-plugin-css-only'
 import vue from 'rollup-plugin-vue' // Handle .vue SFC files
 export default {
   input: 'src/index.js', // Path relative to package.json
-  output: {
-    name: 'vuetify',
-    exports: 'named',
-  },
+  output: [
+    {
+      format: 'es',
+      file: 'dist/vuetify.esm.js',
+    },
+    {
+      name: 'CronVuetify',
+      format: 'umd',
+      file: 'dist/vuetify.umd.js',
+      globals: {
+        vue: 'Vue',
+        '@vue-js-cron/core': 'CronCore',
+      },
+    },
+  ],
   plugins: [
     css({
       output: 'vuetify.css',
